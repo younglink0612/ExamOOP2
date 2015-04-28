df2163ff66e154b4838e7f699d995eeb156a1c36
younglink0612 <younglink0612@yahoo.com.tw>
2015-04-28 18:01:25 +0800

    B AM021983 林少鏞提交 editFile

--- Question2.java
+++ Question2.java
@@ -1,7 +1,11 @@
 ﻿public class Question2{
 	public static void main(String[] args){
-		txtCopy("Source.txt", "Dest.txt");
+		//txtCopy("Source.txt", "Dest.txt");
 		editFile("Dest.txt");
-		updateFile("Dest.txt");
+		//updateFile("Dest.txt");
 	}
 }
+class void editFile(){
+		System.out.println("不會");
+	}
+ } 
